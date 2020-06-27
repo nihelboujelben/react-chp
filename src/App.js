40e8a2f56d94f36431e8d25b19e1280fcf829002
nihelboujelben <nihelboujelben@gmail.com>
2020-06-27 16:16:37 +0100

    Create a React app and publish it to GitHub Pages

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css';
+import { Button, Form } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+<div className="main"> 
+<Form className="form">
+  <Form.Group controlId="formBasicEmail"   >
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+</div>
+  );
+}
+
+export default App;
